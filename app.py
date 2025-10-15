3118251f40b68d9d12da3cbefd9c7992e8490259
AhmedbegggaUA <abh28@alu.ua.es>
2025-10-15 12:43:13 +0200

    Added second commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title="SmilePay - Pagos Biométricos",
     page_icon="😊",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded"  # Cambiado de "expanded" a "auto"
 )
 
 # ==================== ESTADO DE LA APLICACIÓN ====================
